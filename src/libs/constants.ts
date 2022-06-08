0aec38ffcc399244b9924ca5e1bc22d0a68ba281
hochan222 <hochan049@gmail.com>
2022-06-08 18:41:14 +0900

    feat: PERSISTENT_DATA_PATH, inactive workflows

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -2,5 +2,6 @@ const COLLECTED_METRICS = new Map(['bootup-time', 'first-contentful-paint'].map(
 
 const REPORT_PATH = 'report';
 const TEMP_DATA_PATH = 'tempData';
+const PERSISTENT_DATA_PATH = 'persistentData';
 
-export { COLLECTED_METRICS, REPORT_PATH, TEMP_DATA_PATH };
+export { COLLECTED_METRICS, REPORT_PATH, TEMP_DATA_PATH, PERSISTENT_DATA_PATH };
