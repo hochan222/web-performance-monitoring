4a14405a18203672c66899bc657cea3b3bf28955
hochan222 <hochan049@gmail.com>
2022-06-14 02:12:48 +0900

    fix: toFixedTwo, category

--- src/markdown/lhr/categories.ts
+++ src/markdown/lhr/categories.ts
@@ -1,6 +1,6 @@
 import { readFile } from 'fs/promises';
 import { TEMP_DATA_PATH } from '../../libs/constants';
-import { score } from '../../libs/utils';
+import { score, toFixedTwo } from '../../libs/utils';
 import { tAlignLine, tBody, tHead } from '../markdown';
 
 export async function generateCategoryMarkdown(path, categories): Promise<string[]> {
@@ -9,6 +9,6 @@ export async function generateCategoryMarkdown(path, categories): Promise<string
   return [
     tHead(staticCategories.map((category) => category.title)),
     tAlignLine(categories.length, 'center'),
-    tBody(categories.map((category) => `${score(category.score)} ${category.score}`)),
+    tBody(categories.map((category) => `${score(category.score)} ${toFixedTwo(category.score)}`)),
   ];
 }
